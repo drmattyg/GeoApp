386e32c8a99465474a6792c3cce8653654c45d28
Matthew Gordon <mgordon@palantir.com>
2015-07-20 09:56:57 -0700

    Initial Commit

---
+++ First Geo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First Geo
+//
+//  Created by Matthew Gordon on 7/20/15.
+//  Copyright (c) 2015 Dr Matty G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
